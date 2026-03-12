4588cf323d51c217acf98c5c719f085bf7b08517
ownermood2 <ownermood2@github.com>
2026-03-12 08:33:47 +0530

    New MongoDB, SSL fix, latest updates

--- modules/vars.py
+++ modules/vars.py
@@ -13,7 +13,7 @@ CREDIT_LINK = environ.get("CREDIT_LINK", 'https://t.me/CLAT_OWNER')
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
 # MongoDB Configuration
-MONGO_URL = 'mongodb+srv://timep2176_db_user:uqTsQZJ8cggFgRQS@cluster0.ukkb8p4.mongodb.net/?appName=Cluster0'
+MONGO_URL = 'mongodb+srv://moinuddinraza0007_db_user:aBbZbu1SQOtU2EXg@cluster0.kwdpwu8.mongodb.net/?appName=Cluster0'
 DATABASE_NAME = environ.get("DATABASE_NAME", "eagle")
 
 # Owner and Admin Configuration
